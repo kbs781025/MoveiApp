e57847d4c0c5f7324677fbc31f9b4a4a692dad96
kbs781025 <kbs781025@naver.com>
2020-04-11 16:53:42 +0900

    undefined url redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigator from "./Components/Navigator";
@@ -8,8 +8,14 @@ function App() {
   return (
     <HashRouter>
       <Navigator />
-      <Route path="/" exact={true} component={Home}></Route>
-      <Route path="/movie-detail/:id" component={About}></Route>
+      <Switch>
+        <Route path="/" exact={true} component={Home}></Route>
+        <Route path="/movie-detail/:id" component={About}></Route>
+        <Route>
+          {" "}
+          <Redirect to="/"></Redirect>
+        </Route>
+      </Switch>
     </HashRouter>
   );
 }
