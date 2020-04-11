07f102e0911ad4be55d4662be74266eb19f38a45
kbs781025 <kbs781025@naver.com>
2020-04-11 16:57:26 +0900

    delete comments

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,20 +1,6 @@
 import * as React from "react";
 import { RouteComponentProps, Redirect } from "react-router-dom";
 
-// function About(
-//   props: RouteComponentProps<{ id: string }, StaticContext, MovieType>
-// ) {
-//   console.log(props);
-//   const movie: MovieType = props.location.state;
-//   const id: string = props.match.params.id;
-//   return (
-//     <div>
-//       <span>{id}</span>
-//       <span>{movie.title}</span>
-//     </div>
-//   );
-// }
-
 interface movieDetailType {
   title: string;
   year: number;
